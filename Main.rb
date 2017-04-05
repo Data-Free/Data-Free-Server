32e9d0ce23d6bb3e68671ee20a3ad1c7451d739e
darrendawson <thedarrendawson@gmail.com>
2017-04-05 00:21:29 -0700

    Add int_to_key function, response texts now start with 2 char key

--- Main.rb
+++ Main.rb
@@ -18,11 +18,11 @@ get '/message' do
   #body = content of sms minus the botkey
   body = sms[2..-1]
 
-  # botkey = first two chars of sms
-  botKey = sms[0,2]
+  # bot_key = first two chars of sms
+  bot_key = sms[0,2]
 
   # BotFinder uses botkey to return name of program to run
-  bot = %x{ruby BotFinder.rb #{botKey}}
+  bot = %x{ruby BotFinder.rb #{bot_key}}
   bot = bot.chomp
 
   # Runs bot, result contains the bot's output
@@ -39,15 +39,31 @@ get '/message' do
   index = 0
   max = output.length + 1
   while(index<max)
-    smsMessage = output[index]
+    # smsMessage = getKey(index)
+    sms_message = int_to_key(index) + output[index] 
+
+    #send text
     @client = Twilio::REST::Client.new account_sid, auth_token
-    message = @client.account.messages.create(:body => smsMessage,
+    message = @client.account.messages.create(:body => sms_message,
        :to => "+",    # Replace with your phone number "+15555555555"
        :from => "")  # Replace with your Twilio number "+15555555555"
+
     index+=1
   end
   #-----------------------------------------------------
  
+end
+
+#converts an int into base 26(a-z)
+def int_to_key(num)
+  #10s digit = num/26
+  d1 = num/26 + 97
+
+  #1s digit = num%26
+  d2 = num%26 + 97
+  result = d1.chr + d2.chr
+
+  return result
 end
 
 #-------------------------------------------------------------------------
@@ -60,7 +76,7 @@ Receiving User Request
  - sms: String containing message from the user
         formatted as botKey + body
  - body: String with user's request
- - botKey: 2 char String with key identifying which bot user wants
+ - bot_key: 2 char String with key identifying which bot user wants
            The DataFree app automatically adds this key to beginning of sms
 
 Retreiving the Answer
@@ -70,5 +86,5 @@ Retreiving the Answer
 Sending the Answer
  - index: int, increments through all Strings in output
  - max: int, size limit of output
- - smsMessage: String, stores the String at output[index] to send to user
+ - sms_message: String, stores the String at output[index] to send to user
 =end
